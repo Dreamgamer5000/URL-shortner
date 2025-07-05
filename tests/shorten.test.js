ba237436fd3755f8d9de5eb74e9d1bca398107a6
Dreamgamer5000 <antonyrejit@gmail.com>
2025-07-05 11:09:51 +0530

    increased timeout to check tests

--- tests/shorten.test.js
+++ tests/shorten.test.js
@@ -1,5 +1,7 @@
-const request = require("supertest");
+jest.setTimeout(15000); // 15 seconds, do this at the top of the file
+
 const app = require("../app");
+const request = require("supertest");
 
 test("POST /shorten", async () => {
   const res = await request(app).post("/shorten").send({ url: "https://example.com" });
